6799c14ecd07d63db2763353ed3a8ce1054a1e62
Bjoern Meyer <bjoern@textcontrol.com>
2023-10-09 14:50:51 +0200

    Prompt added

--- tx-chatgpt-ai-integration/Controllers/ChatGPTController.cs
+++ tx-chatgpt-ai-integration/Controllers/ChatGPTController.cs
@@ -9,33 +9,101 @@ using System.Text;
 using tx_chatgpt_ai_integration.Models;
 using Newtonsoft.Json;
 using System.Security.Principal;
+using TXTextControl.Web;
 
-namespace tx_chatgpt_ai_integration.Controllers {
-    public class ChatGPTController : Controller {
+namespace tx_chatgpt_ai_integration.Controllers
+{
+    public class ChatGPTController : Controller
+    {
 
-      [HttpPost]
-      public string RequestAPI([FromBody] ChatGPTRequest request) {
+        [HttpPost]
+        public string RequestAPI([FromBody] ChatGPTRequest request)
+        {
 
-         HttpClient client = new HttpClient();  
+            HttpClient client = new HttpClient();
 
-         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "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");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Models.Constants.OPENAI_API_KEY);
 
-         var openAIPrompt = new {
-            model = "text-davinci-003",
-            prompt = Models.Constants.Prompts[request.Type] + request.Text.Trim(),
-            temperature = 0.2,
-            max_tokens = 2048,
-            top_p = 1
-         };
+            var openAIPrompt = new
+            {
+                model = "text-davinci-003",
+                prompt = Models.Constants.Prompts[request.Type] + request.Text.Trim(),
+                temperature = 0.2,
+                max_tokens = 2048,
+                top_p = 1
+            };
 
-         var stringContent = new StringContent(JsonConvert.SerializeObject(openAIPrompt), Encoding.UTF8, "application/json");
-         var result = client.PostAsync("https://api.openai.com/v1/completions", stringContent).Result;
-         var jsonContent = result.Content.ReadAsStringAsync().Result;
-         ChatGPTResponse chatGPTResponse = JsonConvert.DeserializeObject<ChatGPTResponse>(jsonContent);
+            var stringContent = new StringContent(JsonConvert.SerializeObject(openAIPrompt), Encoding.UTF8, "application/json");
+            var result = client.PostAsync("https://api.openai.com/v1/completions", stringContent).Result;
+            var jsonContent = result.Content.ReadAsStringAsync().Result;
+            ChatGPTResponse chatGPTResponse = JsonConvert.DeserializeObject<ChatGPTResponse>(jsonContent);
 
-         return chatGPTResponse.Choices[0].Text.Trim();
+            return chatGPTResponse.Choices[0].Text.Trim();
         }
 
-      
+        [HttpPost]
+        public string PromptRequest([FromBody] ChatGPTRequest request)
+        {
+
+            var htmlSuffix = ". Create the results in formatted HTML format with h1, p and li elements.";
+
+            // Remove a trailing full stop, if present
+            if (request.Text.EndsWith("."))
+            {
+                request.Text = request.Text.TrimEnd('.');
+            }
+
+            HttpClient client = new HttpClient
+            {
+                DefaultRequestHeaders =
+                {
+                    Authorization = new AuthenticationHeaderValue("Bearer", Models.Constants.OPENAI_API_KEY)
+                }
+            };
+
+            Request apiRequest = new Request
+            {
+                Messages = new[]
+                {
+                    new RequestMessage
+                    {
+                         Role = "system",
+                         Content = "You are a helpful assistant."
+                    },
+                    new RequestMessage
+                    {
+                         Role = "user",
+                         Content = request.Text + htmlSuffix
+                    }
+                }
+            };
+
+            StringContent content = new StringContent(
+                System.Text.Json.JsonSerializer.Serialize(apiRequest),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            HttpResponseMessage httpResponseMessage = client.PostAsync(
+                "https://api.openai.com/v1/chat/completions",
+                content
+            ).Result;
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                Response response = System.Text.Json.JsonSerializer.Deserialize<Response>(
+                    httpResponseMessage.Content.ReadAsStringAsync().Result
+                );
+
+                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(response.Choices[0].Message.Content);
+                return System.Convert.ToBase64String(plainTextBytes);
+            }
+
+            return null;
+
+
+        }
+
+
     }
 }
--- tx-chatgpt-ai-integration/Models/ChatGPTRequest.cs
+++ tx-chatgpt-ai-integration/Models/ChatGPTRequest.cs
@@ -1,10 +1,72 @@
-﻿namespace tx_chatgpt_ai_integration.Models {
+﻿using System.Text.Json.Serialization;
+
+namespace tx_chatgpt_ai_integration.Models {
     public class ChatGPTRequest {
       public string Text { get; set; }
       public string Type { get; set; }
    }
 
-   public class ChatGPTResponse {
+    public class RequestMessage
+    {
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+    }
+
+    public class Request
+    {
+        [JsonPropertyName("model")]
+        public string Model { get; set; } = "gpt-3.5-turbo";
+        [JsonPropertyName("max_tokens")]
+        public int MaxTokens { get; set; } = 4000;
+        [JsonPropertyName("messages")]
+        public RequestMessage[] Messages { get; set; }
+    }
+
+    public class Response
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("created")]
+        public int Created { get; set; }
+        [JsonPropertyName("model")]
+        public string Model { get; set; }
+        [JsonPropertyName("usage")]
+        public ResponseUsage Usage { get; set; }
+        [JsonPropertyName("choices")]
+        public ResponseChoice[] Choices { get; set; }
+    }
+
+    public class ResponseUsage
+    {
+        [JsonPropertyName("prompt_tokens")]
+        public int PromptTokens { get; set; }
+        [JsonPropertyName("completion_tokens")]
+        public int CompletionTokens { get; set; }
+        [JsonPropertyName("total_tokens")]
+        public int TotalTokens { get; set; }
+    }
+
+    public class ResponseChoice
+    {
+        [JsonPropertyName("message")]
+        public ResponseMessage Message { get; set; }
+        [JsonPropertyName("finish_reason")]
+        public string FinishReason { get; set; }
+        [JsonPropertyName("index")]
+        public int Index { get; set; }
+    }
+
+    public class ResponseMessage
+    {
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+    }
+
+    public class ChatGPTResponse {
       public string Id { get; set; }
       public Choice[] Choices { get; set; }  
    }
@@ -16,7 +78,7 @@
 
    public class Constants {
       public string OPENAI_MODEL = "text-davinci-003";
-      public string OPENAI_API_KEY = "";
+      public static string OPENAI_API_KEY = "";
 
       public static readonly Dictionary<string, string> Prompts = new Dictionary<string, string>() {
             { "formal", "Return the text after the colon in a more professional way and provide the results as plain text: " },
